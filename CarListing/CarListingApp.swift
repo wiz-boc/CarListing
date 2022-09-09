14cb110e63475d4d04c8e8bfb66da812c24a4bd9
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-09-08 20:31:32 -0500

    First commit

---
+++ CarListing/CarListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListingApp.swift
+//  CarListing
+//
+//  Created by wizz on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CarListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
