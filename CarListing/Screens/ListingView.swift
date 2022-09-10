a1b148b686cdbef419a07f0dd74f49198ed0ad05
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-09-09 21:32:31 -0500

    Rename contentView

--- CarListing/Screens/ListingView.swift
+++ CarListing/Screens/ListingView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ListingView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ListingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ListingView()
     }
 }
